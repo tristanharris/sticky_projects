3c9ec9904f43275b203f836ad9c5b0adf094fc5f
Tristan Harris <tris@mohc.net>
2013-11-26 16:52:13 +0000

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+Redmine::Plugin.register :sticky_projects do
+  name 'Sticky Projects plugin'
+  author 'Tristan Harris'
+	description 'Stay on current project when viewing issues for sub-projects'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+end
+
+
+
